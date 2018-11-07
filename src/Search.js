98b93f8196bfe8ccc3e77127717f6a91d9c83cd0
kiyanac <41528791+kiyanac@users.noreply.github.com>
2018-11-06 18:49:14 -0600

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react'
 import * as BooksAPI from './BooksAPI'
 import {Link} from 'react-router-dom'
 import Books from './Books'
-import Main from './Main'
 
 class Search extends Component {
     state = {
@@ -21,7 +20,7 @@ showResults = (search) => {
       if(searchedBooks.error) {
         this.setState({ searchedBooks: []})
       }else{
-            this.setState({searchedBooks:searchedBooks})
+        this.setState({searchedBooks:searchedBooks, search:search})
       }
     })
   }else{
@@ -33,27 +32,40 @@ showResults = (search) => {
     return (
           <div className="search-books">
             <div className="search-books-bar">
-              <Link to="/" className="close-search"
-	>Close</Link>
+              <Link to="/" className="close-search" 
+		>Close</Link>
               <div className="search-books-input-wrapper">
                 <input type="text" placeholder="Search by title or author"
-                value={this.state.search}
-                onChange={(event) => this.updateSearch(event.target.value)}/>
+value={this.state.search}
+onChange={(event) => this.updateSearch(event.target.value)}/>
+
               </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid">
-              {this.state.searchedBooks.map(searchedBook => (<li key={searchedBook.id}>
-                <Books
-                book={searchedBook}
-                changeShelf={this.props.changeShelf}
-                />
-                </li>
-              ))}
-              </ol>
+{this.state.searchedBooks.map(searchedBook => {
+ 
+  let value = "none";
+ this.props.books.map(books => (
+  books.id === searchedBook.id ?
+  value = books.shelf : ''
+  
+)); 
+ return (<li key={searchedBook.id}>
+ <Books
+ book={searchedBook}
+currentShelf={value}
+       changeShelf={this.props.changeShelf}
+
+	/>
+</li>
+)
+})
+}
+</ol>
             </div>
           </div>
-        )
+        )   
   }
 }
 
