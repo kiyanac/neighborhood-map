436e20858bec7b80fe6eddd60240d37460970367
kiyanac <41528791+kiyanac@users.noreply.github.com>
2018-11-07 19:07:13 -0600

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -4,22 +4,23 @@ import Books from './Books'
 
 class Main extends Component {
   render() {
-    return (
-<div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
+    return ( 
+				<div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
             <div className="list-books-content">
               <div>
                 <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                  {this.props.books.filter(book => book.shelf === 'currentlyReading').map(book => (<li key={book.id}>
-                        <Books
-                                                                                          book={book}
-  changeShelf={this.props.changeShelf}
-currentShelf="currentlyReading"/>
+                  {this.props.books.filter(book => book.shelf === 		'currentlyReading').map(book => (<li key={book.id}>
+                        <Books 
+                          book={book}
+                          changeShelf={this.props.changeShelf}
+                          currentShelf="currentlyReading"
+                        />
                       </li>))}
                     </ol>
                   </div>
@@ -29,11 +30,12 @@ currentShelf="currentlyReading"/>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
             {this.props.books.filter(book => book.shelf === 'wantToRead').map(book => (<li key={book.id}>
-                        <Books book={book}
-changeShelf={this.props.changeShelf}
-currentShelf="wantToRead"/>
+                        <Books 
+                        	book={book}
+                          changeShelf={this.props.changeShelf}
+                          currentShelf="wantToRead"
+                        />
                       </li>))}
-
                     </ol>
                   </div>
                 </div>
@@ -41,11 +43,12 @@ currentShelf="wantToRead"/>
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-
                               {this.props.books.filter(book => book.shelf === 'read').map(book => (<li key={book.id}>
-                        <Books book={book}
-changeShelf={this.props.changeShelf}
-currentShelf="read"/>
+                        <Books 
+                        	book={book}
+                          changeShelf={this.props.changeShelf}
+                          currentShelf="read"
+                        />
                       </li>))}
                     </ol>
                   </div>
@@ -53,12 +56,11 @@ currentShelf="read"/>
               </div>
             </div>
             <div className="open-search">
-              <Link to='/search'
-              >Add a book</Link>
+              <Link to='/search'>Add a book</Link>
             </div>
           </div>
-        )
-      }
-}
+				)
+			}
+	}
 
 export default Main
