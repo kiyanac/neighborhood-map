5590c7eb812796ad9341ebfc19559cdb0c3fbf98
kiyanac <41528791+kiyanac@users.noreply.github.com>
2018-11-07 18:56:45 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Route} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Search from './Search'
 import Main from './Main'
 
-class BooksApp extends React.Component {
-state = {
 
-    books: []
+class BooksApp extends React.Component {
+  state = {
+      books: []
+    }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
   }
 
-componentDidMount() {
-   BooksAPI.getAll().then((books) => {
-      this.setState({ books })
+  changeShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+    BooksAPI.getAll().then((books) => {
+        this.setState({ books })
+      })
     })
-}
-
-changeShelf = (book, shelf) => {
-  BooksAPI.update(book, shelf)
-
-  BooksAPI.getAll().then((books) => {
-      this.setState({ books })
-    })
-}
-
+  }
+  
   render() {
     return (
       <div className="app">
 
-<Route exact path="/" render={() => (
-  <Main
-      books={this.state.books}
-      changeShelf={this.changeShelf}
-  />
-)}
-/>
+        <Route exact path="/" render={() => (
+            <Main 
+              books={this.state.books}
+              changeShelf={this.changeShelf}
+            />
+          )}
+        />
 
-<Route path="/search" render={() => (
- <Search
-       changeShelf={this.changeShelf}
-       books={this.state.books}
-  />
-)}
-/>
+        <Route path="/search" render={() => (
+            <Search 
+              changeShelf={this.changeShelf}
+              books={this.state.books}
 
+            />
+          )}
+        /> 
       </div>
     )
   }
 }
 
 export default BooksApp
+
