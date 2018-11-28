ae9ae92cb920ba56c2553bf9e13438e9112cdbc4
kiyanac <41528791+kiyanac@users.noreply.github.com>
2018-11-28 14:33:58 -0600

    Update SideBar.js

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,49 +1,58 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types'
-import escapeRegExp from 'escape-string-regexp'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import escapeRegExp from "escape-string-regexp";
 //import FourSquare from './API.js';
 
 class SideBar extends Component {
-   static propTypes = {
+  static propTypes = {
     venues: PropTypes.array.isRequired
-  }
+  };
 
   state = {
-    search: '',
-  }
+    search: ""
+  };
 
-onChange = (search) => {
-      this.setState({search: search })
-    }
+  onChange = search => {
+    this.setState({ search: search });
+  };
 
   render = () => {
-    console.log("Props", this.props)
-    let showResults
+    console.log("Props", this.props);
+    let showResults;
     if (this.state.search) {
-      const match = new RegExp(escapeRegExp(this.state.search), 'i')
-      showResults = this.props.venues.filter((venue) => match.test(venue.venue.name))
-    }else {
-      showResults = this.props.venues
+      const match = new RegExp(escapeRegExp(this.state.search), "i");
+      showResults = this.props.venues.filter(venue =>
+        match.test(venue.venue.name),
+                                                   //marker.setVisible(true)
+      );
+    } else {
+      showResults = this.props.venues;
     }
-    
+
     return (
       <div className="side-bar">
-		<div className="search">
-      		<input className="mall-list" type="text" 
-      			placeholder="Filter Locations" 
-      			value={this.state.search}
-      			onChange={(event) => this.onChange(event.target.value)}/>
-		</div> 
+        <div className="search">
+          <input
+            className="mall-list"
+            type="text"
+            placeholder="Filter Locations"
+            value={this.state.search}
+            onChange={event => this.onChange(event.target.value)}
+          />
+        </div>
         <ol className="mall-list">
-      		{showResults.map((venue) => (
-         		<li key={venue.venue.id} className="list-item" onClick={() => this.props.listClick(venue.venue.name)}> 
-					{venue.venue.name}
-				</li>
-))}
-      	</ol>
+          {showResults.map(venue => (
+            <li
+              key={venue.venue.id}
+              className="list-item"
+              onClick={() => this.props.listClick(venue)}>
+              {venue.venue.name}
+            </li>
+          ))}
+        </ol>
       </div>
     );
-  }
+  };
 }
 
 export default SideBar;
