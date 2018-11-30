39dd429b8e4c3ac8d52b3a4591e42cb36a9edc52
kiyanac <41528791+kiyanac@users.noreply.github.com>
2018-11-30 17:45:59 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import App from './App'
 import './index.css'
+import * as serviceWorker from './serviceWorker.js'
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <BrowserRouter><App /></BrowserRouter>,
   document.getElementById('root')
 )
+
+//Help from: Doug Brown
+serviceWorker.register();
